ffe07777a4c3304c48ffea1a9703c382035e8612
Robi <robi.maliqi@hotmail.co.uk>
2022-10-31 17:27:23 +0000

    added a component folder and a nav.js and nav.css file, added some basic code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <div className="App"></div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Form} />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <nav>
+      <h2>MERN</h2>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
